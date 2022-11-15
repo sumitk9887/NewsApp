6a3c8441394c00058f5b7b26a3d4daa1df7f6480
Sumit Kumar <sumit.k9887@gmail.com>
2022-11-16 03:14:18 +0530

    api added

---
+++ lib/constants/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+
+  static const APIKEY = '3e099b93bef64b5d9fdadea154bf1f4a';
+  static const TOPHEADLINES = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=$APIKEY';
+}
