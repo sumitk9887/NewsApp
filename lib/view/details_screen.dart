7fda7247cb780ef8c5119afd4c3b56734e3951b9
Sumit Kumar <sumit.k9887@gmail.com>
2022-11-17 05:41:23 +0530

    back button added

--- lib/view/details_screen.dart
+++ lib/view/details_screen.dart
@@ -15,107 +15,129 @@ class Details extends StatelessWidget {
   Widget build(BuildContext context) {
     ScreenUtil.init(context);
 
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          ClipRRect(
-              // borderRadius: BorderRadius.circular(10),
-              child: Image.network(
-            newsController.articles[index].urlToImage.toString(),
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.cover,
-          )),
-          Container(
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-              color: Colors.black45.withOpacity(0.4),
-              // borderRadius: BorderRadius.circular(10)
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            ClipRRect(
+                // borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+              newsController.articles[index].urlToImage.toString(),
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
+            )),
+            Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                color: Colors.black45.withOpacity(0.4),
+                // borderRadius: BorderRadius.circular(10)
+              ),
             ),
-          ),
-          Positioned(
-            bottom: 9,
-            left: 5,
-            child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(right: 30),
-                      child: Text(
-                        newsController.articles[index].title.toString(),
-                        textAlign: TextAlign.start,
-                        maxLines: 4,
-                        // ignore: prefer_const_constructors
-                        style: GoogleFonts.robotoSlab(
-                          fontWeight: FontWeight.bold,
-                          color: const Color(0xfff2f2f2),
-                          fontSize: 29.sp,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 48,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: newsController.articles[index].author
-                                      ?.toString() ==
-                                  null
-                              ? Text(
-                                  "NA",
-                                  textAlign: TextAlign.start,
-                                  maxLines: 3,
-                                  style: GoogleFonts.robotoSlab(
-                                    color: const Color(0xffbababa),
-                                    fontSize: 20.sp,
-                                  ),
-                                )
-                              : Text(
-                                  newsController.articles[index].author
-                                      .toString(),
-                                  textAlign: TextAlign.start,
-                                  maxLines: 3,
-                                  style: GoogleFonts.robotoSlab(
-                                    color: const Color(0xffbababa),
-                                    fontSize: 20.sp,
-                                  ),
-                                ),
-                        ),
-                        const SizedBox(
-                          width: 18,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(right: 10),
-                          child: Text(
-                            DateFormat("yyyy-MM-dd").format(DateTime.parse(
-                                newsController.articles[index].publishedAt
-                                    .toString())),
-                            style: GoogleFonts.robotoSlab(
-                              color: const Color(0xffbababa),
-                              fontSize: 20.sp,
-                            ),
+            Positioned(
+               top: 9,
+              left: 5,
+              child: Container(
+                decoration:
+                    const BoxDecoration(color: Colors.black54 , shape:BoxShape.circle),
+                child: IconButton(
+                  iconSize: 31.50,
+                  icon: const Icon(Icons.arrow_back_outlined),
+                  color: const Color(0xfff2f2f2),
+                  hoverColor: Colors.black,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 9,
+              left: 5,
+              child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(right: 30),
+                        child: Text(
+                          newsController.articles[index].title.toString(),
+                          textAlign: TextAlign.start,
+                          maxLines: 4,
+                          // ignore: prefer_const_constructors
+                          style: GoogleFonts.robotoSlab(
+                            fontWeight: FontWeight.bold,
+                            color: const Color(0xfff2f2f2),
+                            fontSize: 29.sp,
                           ),
                         ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    Text(
-                      newsController.articles[index].description.toString(),
-                      textAlign: TextAlign.start,
-                      style: GoogleFonts.robotoSlab(
-                        color: const Color(0xffbababa),
-                        fontSize: 14.sp,
                       ),
-                    ),
-                  ],
-                )),
-          ),
-        ],
+                      const SizedBox(
+                        height: 48,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: newsController.articles[index].author
+                                        ?.toString() ==
+                                    null
+                                ? Text(
+                                    "NA",
+                                    textAlign: TextAlign.start,
+                                    maxLines: 3,
+                                    style: GoogleFonts.robotoSlab(
+                                      color: const Color(0xffbababa),
+                                      fontSize: 20.sp,
+                                    ),
+                                  )
+                                : Text(
+                                    newsController.articles[index].author
+                                        .toString(),
+                                    textAlign: TextAlign.start,
+                                    maxLines: 3,
+                                    style: GoogleFonts.robotoSlab(
+                                      color: const Color(0xffbababa),
+                                      fontSize: 20.sp,
+                                    ),
+                                  ),
+                          ),
+                          const SizedBox(
+                            width: 18,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 10),
+                            child: Text(
+                              DateFormat("yyyy-MM-dd").format(DateTime.parse(
+                                  newsController.articles[index].publishedAt
+                                      .toString())),
+                              style: GoogleFonts.robotoSlab(
+                                color: const Color(0xffbababa),
+                                fontSize: 20.sp,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(3),
+                        child: Text(
+                          newsController.articles[index].description.toString(),
+                          textAlign: TextAlign.start,
+                          style: GoogleFonts.robotoSlab(
+                            color: const Color(0xffbababa),
+                            fontSize: 14.sp,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+            ),
+          ],
+        ),
       ),
     );
   }
